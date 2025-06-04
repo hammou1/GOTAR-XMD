9d3817434b72e2dcb8459ad0728f42cffb07d632
hammou1 <110303828+hammou1@users.noreply.github.com>
2025-06-04 12:00:37 +0100

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const message = await conn.sendMessage(from, { text: '*PINGING...*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `> *𝐆𝐎𝐓𝐀𝐑 𝐗𝐌𝐃 SPEED: ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `> *LOUIZA SPEED: ${ping}ms*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
